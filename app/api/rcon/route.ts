8bf49dbf03db1581a4cf00e90609d40833fad791
LoV432 <monib619@gmail.com>
2023-12-02 18:50:36 +0500

    fix rcon empty command

--- app/api/rcon/route.ts
+++ app/api/rcon/route.ts
@@ -3,8 +3,9 @@ import { rcon } from '@/app/lib/rcon';
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
-	if (!body.command && body.command == '')
+	if (!body.command || body.command.trim().length === 0) {
 		return new Response('Command is required', { status: 400 });
+	}
 
 	const res = await rcon.exec(body.command);
 	return new Response(res, {
