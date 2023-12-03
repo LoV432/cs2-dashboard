72adc13eb6321f782875eafcc87ec9e6a4cac93f
LoV432 <monib619@gmail.com>
2023-12-04 02:42:18 +0500

    workflow: fix even more build errors

--- app/lib/rcon.ts
+++ app/lib/rcon.ts
@@ -2,6 +2,6 @@ import { RCON } from '@fabricio-191/valve-server-query';
 
 export const rcon = await RCON({
 	ip: process.env.SERVER_IP,
-	port: parseInt(process.env.RCON_PORT as string),
+	port: parseInt(process.env.RCON_PORT as string) || 27015,
 	password: process.env.RCON_PASSWORD as string
 });
--- app/lib/server.ts
+++ app/lib/server.ts
@@ -2,5 +2,5 @@ import { Server } from '@fabricio-191/valve-server-query';
 
 export const csServer = await Server({
 	ip: process.env.SERVER_IP,
-	port: parseInt(process.env.SERVER_PORT as string)
+	port: parseInt(process.env.RCON_PORT as string) || 27015
 });
