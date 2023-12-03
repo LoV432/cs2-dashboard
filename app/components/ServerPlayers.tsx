98f91f0c0dbc3844706b44ce65a4a0e1acb8e94b
LoV432 <monib619@gmail.com>
2023-12-04 02:05:57 +0500

    add kick player popup

--- app/components/ServerPlayers.tsx
+++ app/components/ServerPlayers.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { Player, parsePlayerData } from '@/app/lib/parse-players';
+import { useEffect, useState, useRef } from 'react';
+import { Player } from '@/app/lib/parse-players';
+import Image from 'next/image';
 
 export default function ServerPlayers({
 	playersPreRendered
@@ -8,6 +9,8 @@ export default function ServerPlayers({
 	playersPreRendered: Player[];
 }) {
 	const [allPlayers, setAllPlayers] = useState(playersPreRendered);
+	const kickPlayerModal = useRef() as React.MutableRefObject<HTMLDialogElement>;
+	const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
 
 	useEffect(() => {
 		const getPlayersInterval = setInterval(() => {
@@ -33,21 +36,100 @@ export default function ServerPlayers({
 				</thead>
 				<tbody>
 					{allPlayers.map((player) => {
-						return <PlayerRow key={player.name} player={player} />;
+						return (
+							<PlayerRow
+								key={player.name}
+								player={player}
+								setSelectedPlayer={setSelectedPlayer}
+								kickPlayerModal={kickPlayerModal}
+							/>
+						);
 					})}
 				</tbody>
 			</table>
+			<KickPlayerPopUp
+				playerId={selectedPlayer?.id || 0}
+				kickPlayerModal={kickPlayerModal}
+			/>
 		</div>
 	);
 }
 
-function PlayerRow({ player }: { player: Player }) {
+function PlayerRow({
+	player,
+	setSelectedPlayer,
+	kickPlayerModal
+}: {
+	player: Player;
+	setSelectedPlayer: (player: Player) => void;
+	kickPlayerModal: React.MutableRefObject<HTMLDialogElement>;
+}) {
 	return (
 		<tr>
 			<td>{player.ping}</td>
 			<td>{player.loss}</td>
 			<td>{player.name}</td>
 			<td>{player.adr.split(':')[0]}</td>
+			<th>
+				<button
+					onClick={() => {
+						setSelectedPlayer(player);
+						kickPlayerModal.current.showModal();
+					}}
+					className="btn btn-ghost btn-xs"
+				>
+					<Image width={20} height={20} src="/trash-outline.svg" alt="Close" />
+				</button>
+			</th>
 		</tr>
+	);
+}
+
+function KickPlayerPopUp({
+	playerId,
+	kickPlayerModal
+}: {
+	playerId: number;
+	kickPlayerModal: React.MutableRefObject<HTMLDialogElement>;
+}) {
+	const kickPlayer = (e: React.MouseEvent<HTMLButtonElement>) => {
+		fetch('/api/rcon', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				command: `kickid ${playerId}`
+			})
+		});
+		closePopUp();
+	};
+
+	function closePopUp() {
+		kickPlayerModal.current.close();
+	}
+
+	return (
+		<dialog ref={kickPlayerModal} className="modal">
+			<div className="modal-box bg-zinc-900">
+				<h3 className="pb-5 text-lg font-bold">Kick Player</h3>
+				<p>Are you sure you want to kick this player?</p>
+				<button onClick={kickPlayer} className="btn btn-error mt-5 w-full">
+					KICK
+				</button>
+				<button onClick={closePopUp} className="btn btn-ghost mt-5 w-full">
+					Cancel
+				</button>
+				<button
+					onClick={closePopUp}
+					className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+				>
+					✕
+				</button>
+			</div>
+			<div className="modal-backdrop bg-zinc-700 opacity-30">
+				<button onClick={closePopUp}>close</button>
+			</div>
+		</dialog>
 	);
 }
