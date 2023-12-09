625588d9f41b04f4e3539f34a76cdb3782164007
LoV432 <monib619@gmail.com>
2023-12-09 21:09:51 +0500

    fix: maxmind script

--- maxmind_download.sh
+++ maxmind_download.sh
@@ -9,14 +9,14 @@ echo 'Updating Maxmind DBs'
 finalDir='./maxmind'
 mkdir -p $finalDir
 
-wget -LO zip-asn.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
+wget -O zip-asn.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
 tar -xf ./zip-asn.tar.gz -C ./
 cp ./GeoLite2-ASN*/GeoLite2-ASN.mmdb $finalDir/GeoLite2-ASN.mmdb
 
 rm zip-asn.tar.gz
 rm -r GeoLite2*
 
-wget -LO zip-city.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
+wget -O zip-city.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz"
 tar -xf ./zip-city.tar.gz -C ./
 cp ./GeoLite2-City*/GeoLite2-City.mmdb $finalDir/GeoLite2-City.mmdb
 
