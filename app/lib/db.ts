0434e40257a6b7969c0b1fb4333c195109d48cde
LoV432 <monib619@gmail.com>
2024-01-11 20:16:31 +0500

    add port to db

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,11 @@
 import mysql from 'mysql2';
 
-export const db = mysql.createPool({
-	host: process.env.SQL_HOST,
-	user: process.env.SQL_USER,
-	database: process.env.SQL_DB,
-	password: process.env.SQL_PASSWORD
-}).promise();
+export const db = mysql
+	.createPool({
+		host: process.env.SQL_HOST,
+		port: Number(process.env.SQL_PORT),
+		user: process.env.SQL_USER,
+		database: process.env.SQL_DB,
+		password: process.env.SQL_PASSWORD
+	})
+	.promise();
