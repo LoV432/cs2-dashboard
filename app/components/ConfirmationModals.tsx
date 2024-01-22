8c9c69b113793af95e7dca8beb268bc1b8290b65
LoV432 <monib619@gmail.com>
2024-01-23 00:51:58 +0500

    add make vip code

--- app/components/ConfirmationModals.tsx
+++ app/components/ConfirmationModals.tsx
@@ -20,7 +20,7 @@ export function ConfirmationModal({
 				<h3 className="pb-5 text-lg font-bold capitalize">
 					{modalName} Player
 				</h3>
-				<p className='break-all'>
+				<p className="break-all">
 					Are you sure you want to {modalName} "{playerName}"?
 				</p>
 				<button onClick={modalAction} className="btn btn-error mt-5 w-full">
@@ -67,7 +67,7 @@ export function ConfirmationModalWithInput({
 				<h3 className="pb-5 text-lg font-bold capitalize">
 					{modalName} Player
 				</h3>
-				<p className='break-all'>
+				<p className="break-all">
 					Are you sure you want to {modalName} "{playerName}"?
 				</p>
 				<input
@@ -90,6 +90,71 @@ export function ConfirmationModalWithInput({
 					className="btn btn-error mt-5 w-full"
 				>
 					{modalName.toUpperCase()}
+				</button>
+				<button onClick={closePopUp} className="btn btn-ghost mt-5 w-full">
+					Cancel
+				</button>
+				<button
+					onClick={closePopUp}
+					className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+				>
+					✕
+				</button>
+			</div>
+			<div className="modal-backdrop bg-zinc-700 opacity-30">
+				<button onClick={closePopUp}>close</button>
+			</div>
+		</dialog>
+	);
+}
+
+export function ConfirmationModalVip({
+	modalRef,
+	playerName,
+	modalAction
+}: {
+	modalRef: React.MutableRefObject<HTMLDialogElement>;
+	playerName: string;
+	modalAction: (time: number, group: string) => void;
+}) {
+	const timeRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+	const groupRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+	function closePopUp() {
+		modalRef.current.close();
+		timeRef.current.value = '';
+		groupRef.current.value = '';
+	}
+	return (
+		<dialog ref={modalRef} className="modal">
+			<div className="modal-box bg-zinc-900">
+				<h3 className="pb-5 text-lg font-bold capitalize">
+					Make Player VIP
+				</h3>
+				<p className="break-all">
+					Are you sure you want to make "{playerName}" VIP?
+				</p>
+				<input
+					ref={timeRef}
+					className="input mt-5 w-full placeholder:text-slate-500"
+					placeholder="Time in minutes/0 perm"
+				></input>
+				<input
+					ref={groupRef}
+					className="input mt-5 w-full placeholder:text-slate-500"
+					placeholder="VIP Group*"
+				></input>
+				<button
+					onClick={() => {
+						if (groupRef.current.value == null || groupRef.current.value == '')
+							return;
+						modalAction(
+							Number(timeRef.current.value) | 0,
+							String(groupRef.current.value)
+						);
+					}}
+					className="btn btn-success mt-5 w-full"
+				>
+					MAKE VIP
 				</button>
 				<button onClick={closePopUp} className="btn btn-ghost mt-5 w-full">
 					Cancel
