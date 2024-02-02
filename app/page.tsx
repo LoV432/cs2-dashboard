50e27d15f429e698f8b612930ec768b25249d2e9
LoV432 <monib619@gmail.com>
2024-02-02 23:21:16 +0500

    loading ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ServerInfoPanel from './components/ServerInfoPanel.Server';
 import CommandsBox from './components/CommandsBox';
 import RecoilRootWrapper from './components/RecoilRootWrapper';
+import Loading from './components/Loading';
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
 		<>
 			<div className="flex min-h-screen w-full flex-row flex-wrap justify-evenly">
 				<RecoilRootWrapper>
+					<Loading />
 					<ServerInfoPanel />
 					<CommandsBox />
 				</RecoilRootWrapper>
