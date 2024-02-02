d710765720bd001f1e95ab72e2c39017406701df
LoV432 <monib619@gmail.com>
2024-02-02 15:00:38 +0500

    remove duplicate code

--- app/api/server-info/route.ts
+++ app/api/server-info/route.ts
@@ -1,42 +1,7 @@
-export const dynamic = 'force-dynamic';
-import { csServerInit } from '@/app/lib/server';
-import type { Server } from '@fabricio-191/valve-server-query';
-
-//@ts-ignore
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
-// The return value of Server.getInfo() contains BigInt properties
-// JSON.stringify() cannot serialize BigInts by default
-BigInt.prototype.toJSON = function () {
-	return this.toString();
-};
-
-let lastReqTime = 0;
-let serverInfo: Server.Info;
+import { getServerInfo } from '@/app/lib/get-server-info';
 
 export async function GET() {
-	if (Date.now() - lastReqTime < 5000) {
-		return new Response(JSON.stringify(serverInfo), {
-			headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*'
-			}
-		});
-	}
-	lastReqTime = Date.now();
-	const csServer = await csServerInit();
-	if ('err' in csServer) {
-		console.log(csServer.err);
-		//TODO: Handle this on frontend
-		return new Response(JSON.stringify({ err: 'error' }), {
-			headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*'
-			}
-		});
-	}
-	serverInfo = await csServer.getInfo();
-	csServer.disconnect();
-	return new Response(JSON.stringify(serverInfo), {
+	return new Response(JSON.stringify(await getServerInfo()), {
 		headers: {
 			'Content-Type': 'application/json',
 			'Access-Control-Allow-Origin': '*'
