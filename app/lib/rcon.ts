6a657e4b165ce2340378b43057bccf3f27030089
LoV432 <monib619@gmail.com>
2024-02-19 14:03:42 +0500

    add timeout to rcon commands

--- app/lib/rcon.ts
+++ app/lib/rcon.ts
@@ -1,12 +1,13 @@
 import { RCON } from '@fabricio-191/valve-server-query';
 
 //TODO: Is this really the best way to do this?
-export const rconInit = async (ip : string, port : number, password : string) => {
+export const rconInit = async (ip: string, port: number, password: string) => {
 	try {
 		return await RCON({
 			ip,
 			port,
-			password
+			password,
+			timeout: 500
 		});
 	} catch (err) {
 		return { err };
