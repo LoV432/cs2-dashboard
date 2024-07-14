962c9166893669592a4e12a0371a94ac41aab17a
LoV432 <monib619@gmail.com>
2024-07-14 22:16:43 +0500

    switch to permanent redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 
 export default function Page() {
-	redirect('/server/0');
+	permanentRedirect('/server/0');
 }
